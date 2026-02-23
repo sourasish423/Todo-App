6ffb12b40fa91dbe5bd57538aa09edc9ce576ee6
Sourasish Das <dassourasish200@gmail.com>
2026-02-24 00:05:24 +0530

    Todo UI completed

---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import tick from '../assets/tick.png'
+import not_tick from '../assets/not_tick.png'
+import delete_icon from '../assets/delete.png'
+
+const TodoItems = ({text}) => {
+  return (
+    <div className='flex items-center my-3 gap-2'>
+
+        <div className='flex flex-1 items-center cursor-pointer'>
+            <img src={tick} alt=""  className='w-7'/>
+            <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
+        </div>
+
+        <img src={delete_icon} alt="" className='w-5 cursor-pointer'/>
+      
+
+        <div>
+
+        </div>
+        </div>
+  )
+}
+
+export default TodoItems
