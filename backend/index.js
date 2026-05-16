0f138db46f7b0fd084629a086a4ca15243ed2e21
Sourasish Das <dassourasish200@gmail.com>
2026-05-16 21:37:01 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -1,160 +1,140 @@
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-
-// const Task = require("./models/task");
-// const User = require("./models/user");
-// const auth = require("./middleware/auth");
-
-// const app = express();
-
-// app.use(cors({
-//   origin: "https://todo-app-psi-mauve.vercel.app",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   credentials: true
-// }));
-
-// app.options("*", cors());
-
-// app.use(express.json());
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch(err => console.log(err));
-
-// //for user signup
-// app.post("/signup", async (req, res) => {
-//   try {
-//     const { name, email, password } = req.body;
-
-//     const existingUser = await User.findOne({ email });
-
-//     if (existingUser) {
-//       return res.status(400).json({ message: "User already exists" });
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     await User.create({
-//       name,
-//       email,
-//       password: hashedPassword,
-//     });
-
-//     res.json({ message: "Signup successful" });
-
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: "Server Error" });
-//   }
-// });
-
-// app.post("/login", async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     const foundUser = await User.findOne({ email });
-
-//     if (!foundUser) {
-//       return res.status(400).json({ message: "Invalid credentials" });
-//     }
-
-//     const isMatch = await bcrypt.compare(password, foundUser.password);
-
-//     if (!isMatch) {
-//       return res.status(400).json({ message: "Invalid credentials" });
-//     }
-
-//     const token = jwt.sign(
-//       { id: foundUser._id },
-//       process.env.JWT_SECRET
-//     );
-
-//     res.json({
-//       message: "Login successful",
-//       token
-//     });
-
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: "Server Error" });
-//   }
-// });
-
-// //get all tasks
-// app.get("/tasks", auth, async (req, res) => {
-//   const tasks = await Task.find({
-//     userId: req.user.id
-//   });
-//   res.json(tasks);
-// });
-
-// //add a new task
-// app.post("/tasks", auth, async (req, res) => {
-//   const text = req.body.text;
-
-//   if (!text || text.trim() === "") {
-//     return res.status(400).json({ error: "Task text required" });
-//   }
-
-//   const newTask = await Task.create({
-//     text: text,
-//     iscomplete: false,
-//     userId: req.user.id // links the task to the logged-in user
-//   });
-
-//   res.json(newTask);
-// });
-
-// //for toggling the completion status of a task
-// app.put("/tasks/:id", auth, async (req, res) => {
-//   const task = await Task.findById(req.params.id);
-
-//   if (!task) {
-//     return res.status(404).json({ message: "Not found" });
-//   }
-
-//   task.iscomplete = !task.iscomplete;
-
-//   await task.save();
-
-//   res.json(task);
-// });
-
-// //delete a task by ID
-// app.delete("/tasks/:id", auth, async (req, res) => {
-//   await Task.findByIdAndDelete(req.params.id);
-//   res.json({ message: "Deleted" });
-// });
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-
+const dotenv = require("dotenv");
+dotenv.config();
 
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const Task = require("./models/task");
+const User = require("./models/user");
+const auth = require("./middleware/auth");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://todo-app-psi-mauve.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
-app.get("/", (req, res) => {
-  res.send("Backend works");
+app.options("*", cors());
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+//for user signup
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.json({ message: "Signup successful" });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
-app.post("/login", (req, res) => {
-  res.json({ message: "login route works" });
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const foundUser = await User.findOne({ email });
+
+    if (!foundUser) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, foundUser.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: foundUser._id },
+      process.env.JWT_SECRET
+    );
+
+    res.json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
+//get all tasks
+app.get("/tasks", auth, async (req, res) => {
+  const tasks = await Task.find({
+    userId: req.user.id
+  });
+  res.json(tasks);
+});
+
+//add a new task
+app.post("/tasks", auth, async (req, res) => {
+  const text = req.body.text;
+
+  if (!text || text.trim() === "") {
+    return res.status(400).json({ error: "Task text required" });
+  }
+
+  const newTask = await Task.create({
+    text: text,
+    iscomplete: false,
+    userId: req.user.id // links the task to the logged-in user
+  });
+
+  res.json(newTask);
+});
+
+//for toggling the completion status of a task
+app.put("/tasks/:id", auth, async (req, res) => {
+  const task = await Task.findById(req.params.id);
+
+  if (!task) {
+    return res.status(404).json({ message: "Not found" });
+  }
+
+  task.iscomplete = !task.iscomplete;
+
+  await task.save();
+
+  res.json(task);
+});
+
+//delete a task by ID
+app.delete("/tasks/:id", auth, async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+});
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log(`Server running on port ${PORT}`);
 });
+
+
+
